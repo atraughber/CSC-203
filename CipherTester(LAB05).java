3b8db9a10081df89e575bd3e112234fbb1a714d8
Anna <annatraughber@gmail.com>
2018-03-03 18:43:56 -0500

    Create CipherTester(LAB05).java

---
+++ CipherTester(LAB05).java
@@ -0,0 +1,59 @@
+/**
+ * This program tests the CaesarCipher class, which includes 
+ * encrypt and decrypt methods.  
+ * 
+ * @author (Anna Traughber) 
+ * @version (February 28, 2018)
+ */
+import java.util.*; 
+public class CipherTester
+{
+    public static void main (String[] args)
+    {
+        int key;       // key for shift 
+        CaesarCipher sender, receiver;         
+        String plainText, cipherText; 
+        char response; // user response to continue or not 
+        Scanner input = new Scanner (System.in); // to read user input
+
+        // display purpose
+        System.out.println ("This program tests the CaesarCipher class, "
+            + "which includes," ); 
+        System.out.println ("encrypt and decrypt methods."); 
+        System.out.println (); // print blank line
+        System.out.println ("Author: Anna Traughber\tVersion: February 28, 2018"); 
+        System.out.println (); // print blank line
+
+        // prompt to see if user wants to run the program 
+        System.out.print ("Want to run? (enter y to run): "); 
+        response = input.next().charAt(0); 
+        System.out.println (); // print blank line
+
+        while (response == 'y' || response == 'Y') { 
+            System.out.println ("Enter non-negative int key "
+                + "and input string, all in one line"); 
+            key = input.nextInt(); // read int value for key
+            plainText = input.nextLine(); // read input string
+            System.out.println (); // print blank line
+            // create sender and receiver     
+            sender = new CaesarCipher(key);
+            receiver = new CaesarCipher(key);
+            System.out.println ("Input to sender:    " + plainText); 
+            // Make the sender encrypt plainText and assign the returned String to cipherText
+            cipherText = sender.encrypt(plainText);             
+            System.out.println ("Output of sender:   " + cipherText); 
+            System.out.println ("Sender transmits encrypted text to receiver"); 
+            System.out.println ("Input to receiver:  " + cipherText); 
+            // Make the receiver decrypt ciphertext and assign the returned String to plainText
+            plainText = receiver.decrypt(cipherText);
+            System.out.println ("Output of receiver: " + plainText); 
+            System.out.println (); // print blank line
+            // prompt to see if user wants to tun the program 
+            System.out.print ("Want to run? (enter y to run): "); 
+            response = input.next().charAt(0); 
+            System.out.println (); // print blank line
+        }
+        System.out.println ("Program has terminated."); 
+        System.out.println (); // print blank line
+    }  
+}
