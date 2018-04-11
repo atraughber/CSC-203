04d0dc5b12098407c9533ea299c8b5afda119566
Anna <annatraughber@gmail.com>
2018-04-11 14:09:04 -0400

    Update Distance(PROG03).java

--- Distance(PROG03).java
+++ Distance(PROG03).java
@@ -11,23 +11,24 @@ public class Distance
     // constant variables
     public static final int YARDS_IN_MILE = 1760;
     public static final int FEET_IN_YARD = 3;    
-    
+
     // instance variables
     private int miles;
     private int yards;
     private int feet;
-    
+
     /**
      * Constructor for objects of class Distance
      */
     public Distance(int miles, int yards, int feet)
     {
         // initialise instance variables
-        assert((miles >= 0) && (yards >= 0) && (feet >= 0) && (yards <= YARDS_IN_MILE) && (feet <= FEET_IN_YARD));
+        assert((miles >= 0) && (yards >= 0) && (feet >= 0) && (yards < YARDS_IN_MILE) && (feet < FEET_IN_YARD));
         this.miles = miles;
         this.yards = yards;
         this.feet = feet; 
     }
+
     /**
      * Method that finds out if there was a valid Distance inputted.
      * 
@@ -38,10 +39,11 @@ public class Distance
      */
     public static boolean isValidDistance(int miles, int yards, int feet) 
     {
-        if(miles < 0 || yards < 0 || feet < 0)
+        if((miles < 0 || yards < 0 || feet < 0) || (yards >= YARDS_IN_MILE || feet >= FEET_IN_YARD))
             return false;
         return true;    
     }
+
     /**
      * Getter methods
      *
@@ -52,14 +54,17 @@ public class Distance
     {
         return miles;
     }
+
     public int getYards()
     {
         return yards;
     }
+
     public int getFeet()
     {
         return feet;
     }
+
     /**
      * Adds the distance values and returns the total distance.
      * 
@@ -68,27 +73,28 @@ public class Distance
      */
     public Distance addDistance(Distance bDistance)
     {
-      int totalMiles;
-      int totalYards;
-      int totalFeet;
-      totalMiles = bDistance.getMiles() + this.miles;
-      totalYards = bDistance.getYards() + this.yards;
-      totalFeet = bDistance.getFeet() + this.feet;
-      if(totalFeet >= FEET_IN_YARD) {
-          totalFeet = totalFeet - FEET_IN_YARD;
-          totalYards = totalYards + 1;
-      }
-      if(totalYards >= YARDS_IN_MILE) {
-          totalYards = totalYards - YARDS_IN_MILE;
-          totalMiles = totalMiles + 1;
-      }      
-    return new Distance(totalMiles, totalYards, totalFeet);
+        int totalMiles;
+        int totalYards;
+        int totalFeet;
+        totalMiles = bDistance.getMiles() + this.miles;
+        totalYards = bDistance.getYards() + this.yards;
+        totalFeet = bDistance.getFeet() + this.feet;
+        if(totalFeet >= FEET_IN_YARD) {
+            totalFeet = totalFeet - FEET_IN_YARD;
+            totalYards = totalYards + 1;
+        }
+        if(totalYards >= YARDS_IN_MILE) {
+            totalYards = totalYards - YARDS_IN_MILE;
+            totalMiles = totalMiles + 1;
+        }      
+        return new Distance(totalMiles, totalYards, totalFeet);
     }
+
     /**
      * toString method
      */
     public String toString()    
     {
-       return(miles + "m:" + yards + "y:" + feet + "f"); 
+        return(miles + "m:" + yards + "y:" + feet + "f"); 
     }
 }
