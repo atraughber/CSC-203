96247fc6b574b615d7d2ce0b1232bcb6c70d881e
Anna <annatraughber@gmail.com>
2018-04-11 14:08:29 -0400

    Update TravelTracker(PROG03).java

--- TravelTracker(PROG03).java
+++ TravelTracker(PROG03).java
@@ -17,22 +17,30 @@ public class TravelTracker
         Scanner input = new Scanner (System.in);  // to read user input
 
         // display purpose and author
-        System.out.println ("This program tracks total distance in miles,
-        System.out.println ("single line with spaces in between.  Entering all
-        System.out.println ("zeroes will terminate the program.");
+        System.out.println ("This program tracks total distance in miles, ");
+        System.out.println ("yards and feet covered in a trip. Invalid values ");
+        System.out.println ("lead to error messages.");
         System.out.println (); // print blank line
-        
+        System.out.println ("Author: Anna Traughber\tVersion: April 08, 2018"); 
+        System.out.println (); // print blank line
+
+        // specify format for input
+        System.out.println ("Enter distance in miles, yards and feet, all in a ");
+        System.out.println ("single line with spaces in between.  Entering all ");
+        System.out.println ("zeros will terminate the program.");
+        System.out.println (); // print blank line
+
         // create the totalDistance object of type Distance with 0 miles, 0 yards, and 0 feet
         totalDistance = new Distance(0, 0, 0);
         System.out.println ("Distance covered so far is: " + totalDistance.toString());
         System.out.println (); // print blank line
-        
+
         // prompt and read distance for a day
         System.out.print ("Enter distance covered for a day: ");
         miles = input.nextInt();
         yards = input.nextInt();
         feet = input.nextInt();
-        
+
         // accumulate if appropriate
         while(miles != 0 || yards != 0 || feet != 0) {           
             if(Distance.isValidDistance(miles, yards, feet)) {
@@ -43,7 +51,7 @@ public class TravelTracker
                 System.out.println ("Distance covered so far is: " + totalDistance.toString());
                 System.out.println (); // print blank line
             } else {
-                System.out.print("**Invalid value.  Not processed.
+                System.out.print("**Invalid value.  Not processed.");
                 System.out.println (); // print blank line
                 System.out.println (); // print blank line
             }    
@@ -52,8 +60,8 @@ public class TravelTracker
             miles = input.nextInt();
             yards = input.nextInt();
             feet = input.nextInt();
-         }
-        
+        }
+
         // wrap up and print final distance
         if((miles == 0) && (yards == 0) && (feet == 0)) {           
             System.out.println (); // print blank line
@@ -64,4 +72,3 @@ public class TravelTracker
         }
     }     
 }
-
